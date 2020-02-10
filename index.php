1ab43438845c2dd32535be90f1bf3a32c8d51d0f
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-02-10 16:15:58 +0100

    .

--- index.php
+++ index.php
@@ -7,12 +7,50 @@ require 'vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->addBodyParsingMiddleware();
+
+$app->addRoutingMiddleware();
 // Add Error Handling Middleware
 $app->addErrorMiddleware(true, false, false);
 
-$app->get('/slim/hello', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello World');
+$app->get('/slim/hello/{Franck}', function (Request $request, Response $response , array $args) {
+    $name = $args['Franck'];
+    $response->getBody()->write('Hello'.' '.$name);
     return $response;
+});
+
+$app->get('/slim/users', function (Request $request, Response $response , array $args) {
+
+$users = [
+    ['name' => 'Franck', 'age' => 31],
+    ['name' => 'Vincent', 'age' => 29],
+    ['name' => 'Thibaut', 'age' => 22]
+];
+
+$payload = json_encode($users);
+$newResponse = $response->withHeader('Content-type', 'application/json');
+$newResponse->getBody()
+            ->write($payload);
+
+return $newResponse;
+
+});       
+
+$app->post('/slim/users', function (Request $request, Response $response , array $args) {
+$jsonBody = $request->getParsedBody();
+$name = $jsonBody['name'];
+$age = $jsonBody['age'];
+
+$data = [ 'msg' => 'ok', 'name' => $name, 'age' => $age];
+$payload = json_encode($data);
+
+$newResponse = $response->withHeader('Content-type', 'application/json');
+$newResponse->getBody()
+            ->write($payload);
+
+
+return $newResponse;
+
 });
 
 $app->run();
